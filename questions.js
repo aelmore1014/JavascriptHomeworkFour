6465a9edeafdff59f8c956ef44852ae4a59e0615
Alex Elmore <alexelmore1014@gmail.com>
2019-11-12 02:24:29 -0500

    Created the HTML layout and added variables to match

--- questions.js
+++ questions.js
@@ -10,8 +10,23 @@ var questions = [
       answer: "parentheses"
     },
     {
-        tile: "Arrays in Javasciprt cant be used to store _______."
-        choices: [""]
+        tile: "Arrays in Javasciprt can be used to store _______.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
     }
   ];
-  
+
+var startBtn = document.querySelector(".startButton");
+var highScores = document.querySelector("#highScores");
+var timerEl = document.querySelector("#timer");
+var displayQuestions = document.querySelector("#questions");
+var displayAnswers = document.querySelector("#answers");
+var gameOver = document.querySelector("#gameOver");
+var endingScore = document.querySelector("endingScore");
+var initials = document.querySelector("#initialsInput");
+var initialSubmit = document.querySelector("#initialSubmit");
+var wrongOrRight = document.querySelector("#wrongOrRight");
+var highScoresDiv = document.querySelector("#highScoresDiv");
+var clearScores = document.querySelector("#clear");
+var highScoresDisplay = document.querySelector("#highScoresDisplay");
+
